3bb9ed2d1293a3813e044ad964edff7f7fb0ec7d
aymen <aymen.sellaouti@gmail.com>
2019-04-22 11:57:48 +0100

    binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
-
+import { SecondComponent } from './second.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -2,14 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first',
-  templateUrl: './first.component.html',
+  templateUrl: 'first.component.html',
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent implements OnInit {
-
+  name = 'sofrecom';
+  private isShown = true;
+  message = 'afficher';
   constructor() { }
 
   ngOnInit() {
   }
-
+  getIsShown() {
+    return this.isShown;
+  }
+  toogle(maVar) {
+    console.log(maVar.innerHTML);
+    this.isShown = !this.isShown;
+    (this.message === 'afficher') ? this.message = 'cacher' : this.message = 'afficher';
+  }
 }
